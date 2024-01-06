89e1ac97b8956548f73354ff2a6dd4c25efee3d9
Truong Tien Dat <dd220302@gmail.com>
2024-01-06 15:10:35 +0700

    remove DistrictId and ProvinceId in WardDto and DistrictDto

--- CamAISolution/Core.Domain/Models/DTO/Districts/DistrictDto.cs
+++ CamAISolution/Core.Domain/Models/DTO/Districts/DistrictDto.cs
@@ -5,7 +5,5 @@ namespace Core.Domain.Models.DTO;
 public class DistrictDto : BaseDto
 {
     public string Name { get; set; } = null!;
-    public Guid ProvinceId { get; set; }
-
     public ProvinceDto Province { get; set; } = null!;
 }
--- CamAISolution/Core.Domain/Models/DTO/Wards/WardDto.cs
+++ CamAISolution/Core.Domain/Models/DTO/Wards/WardDto.cs
@@ -5,7 +5,5 @@ namespace Core.Domain.DTO;
 public class WardDto : BaseDto
 {
     public string Name { get; set; } = null!;
-    public Guid DistrictId { get; set; }
-
     public DistrictDto District { get; set; } = null!;
 }
