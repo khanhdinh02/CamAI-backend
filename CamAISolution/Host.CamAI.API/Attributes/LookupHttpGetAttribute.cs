4a52b3268e3a4bd44f3fd71cd515435dcb03e91b
ngud <nguyenduduy233@gmail.com>
2024-01-08 11:46:45 +0700

    #316 Lookup Controller + #315 Lookup endpoint

---
+++ CamAISolution/Host.CamAI.API/Attributes/LookupHttpGetAttribute.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Host.CamAI.API.Attributes;
+
+public class LookupHttpGetAttribute(string template, Type type) : HttpGetAttribute(template)
+{
+    public Type Type { get; } = type;
+}
