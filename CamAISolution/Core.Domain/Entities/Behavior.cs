5532cece0262739601d433648ca2c8dc622f9b4a
Tran Dinh Khanh <khanhdinh02@yahoo.com>
2024-01-27 14:57:38 +0700

    Edit Behavior entity

--- CamAISolution/Core.Domain/Entities/Behavior.cs
+++ CamAISolution/Core.Domain/Entities/Behavior.cs
@@ -5,12 +5,12 @@ namespace Core.Domain.Entities;
 public class Behavior : BusinessEntity
 {
     public int BehaviorTypeId { get; set; }
-    public Guid? AccountId { get; set; }
+    public Guid? EmployeeId { get; set; }
     public Guid? CameraId { get; set; }
     public DateTime Time { get; set; }
 
     public virtual BehaviorType BehaviorType { get; set; } = null!;
-    public virtual Account Account { get; set; } = null!;
-    public virtual Camera Camera { get; set; } = null!;
+    public virtual Employee? Employee { get; set; }
+    public virtual Camera? Camera { get; set; }
     public virtual ICollection<Evidence> Evidences { get; set; } = new HashSet<Evidence>();
 }
