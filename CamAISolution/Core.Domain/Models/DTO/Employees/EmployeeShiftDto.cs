55c9c7788cd703b1b1a58f8cc9f07941c02464c0
Tran Dinh Khanh <khanhdinh02@yahoo.com>
2024-02-05 21:11:41 +0700

    Update DTO

--- CamAISolution/Core.Domain/Models/DTO/Employees/EmployeeShiftDto.cs
+++ CamAISolution/Core.Domain/Models/DTO/Employees/EmployeeShiftDto.cs
@@ -5,16 +5,10 @@ namespace Core.Domain.DTO;
 
 public class EmployeeShiftDto
 {
-    [Required]
     public Guid EmployeeId { get; set; }
-
-    [Required]
     public Guid ShiftId { get; set; }
 
     [JsonConverter(typeof(JsonStringEnumConverter))]
-    [Required]
     public ShortDayOfWeek DayOfWeek { get; set; }
-
-    [Required]
     public ShiftDto Shift { get; set; } = null!;
 }
