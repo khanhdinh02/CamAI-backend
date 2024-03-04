85ba62795800e07c7d01d811036dd23d2a5e932b
duy <nguyenduduy233@gmail.com>
2024-03-04 07:48:21 +0700

    #519 Hopefully it works

--- CamAISolution/Core.Domain/Models/DTO/Notifications/NotifcationDto.cs
+++ CamAISolution/Core.Domain/Models/DTO/Notifications/NotifcationDto.cs
@@ -4,14 +4,9 @@ namespace Core.Domain.DTO;
 
 public class NotificationDto : BaseDto
 {
-    public NotificationDto()
-    {
-        SentTo = new HashSet<AccountDtoWithBrand>();
-    }
-
     public string Title { get; set; } = null!;
     public string Content { get; set; } = null!;
-    public AccountDtoWithBrand SentBy { get; set; } = null!;
+    public AccountDto SentBy { get; set; } = null!;
     public NotificationStatus Status { get; set; }
-    public ICollection<AccountDtoWithBrand> SentTo { get; set; }
+    public ICollection<AccountDto> SentTo { get; set; } = new HashSet<AccountDto>();
 }
