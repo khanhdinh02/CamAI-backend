b0ac6e515ee3365aa30ce99a9aea582afb0cdcb9
duy <nguyenduduy233@gmail.com>
2024-03-07 21:16:49 +0700

    #523 Update incident and evidence model

--- CamAISolution/Core.Domain/Entities/Incident.cs
+++ CamAISolution/Core.Domain/Entities/Incident.cs
@@ -7,7 +7,10 @@ public class Incident : BusinessEntity
 {
     public IncidentType IncidentType { get; set; }
     public DateTime Time { get; set; }
+    public Guid EdgeBoxId { get; set; }
+    public IncidentStatus Status { get; set; } = IncidentStatus.New;
 
+    public virtual EdgeBox EdgeBox { get; set; } = null!;
     public virtual IEnumerable<Employee> Employees { get; set; } = new HashSet<Employee>();
     public virtual ICollection<Evidence> Evidences { get; set; } = new HashSet<Evidence>();
 }
---
+++ CamAISolution/Core.Domain/Enums/IncidentStatus.cs
@@ -0,0 +1,8 @@
+namespace Core.Domain.Enums;
+
+public enum IncidentStatus
+{
+    New = 0,
+    Accepted = 1,
+    Rejected = 2
+}
