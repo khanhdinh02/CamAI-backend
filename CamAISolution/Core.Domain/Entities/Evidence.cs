c63fef5500f99d438a36591d45dad968e51c1194
duy <nguyenduduy233@gmail.com>
2024-03-10 17:06:06 +0700

    #538 Update model

--- CamAISolution/Core.Domain/Entities/Evidence.cs
+++ CamAISolution/Core.Domain/Entities/Evidence.cs
@@ -9,13 +9,13 @@ public class Evidence : BusinessEntity
     public EvidenceType EvidenceType { get; set; }
     public Guid IncidentId { get; set; }
     public Guid CameraId { get; set; }
-    public Guid ImageId { get; set; }
+    public Guid? ImageId { get; set; }
 
     [JsonIgnore]
     public string EdgeBoxPath { get; set; } = null!;
     public EvidenceStatus Status { get; set; }
 
-    public virtual Image Image { get; set; } = null!;
+    public virtual Image? Image { get; set; }
     public virtual Incident Incident { get; set; } = null!;
     public virtual Camera Camera { get; set; } = null!;
 }
