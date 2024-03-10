09dcb9cc2ba17a3c5cb0708daff04a943891b260
duy <nguyenduduy233@gmail.com>
2024-03-10 14:33:58 +0700

    #523 Update model

---
+++ CamAISolution/Infrastructure.Repositories/Migrations/20240310073113_UpdateEvidenceImage.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEvidenceImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Uri",
+                table: "Evidences");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EdgeBoxPath",
+                table: "Evidences",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ImageId",
+                table: "Evidences",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Evidences_ImageId",
+                table: "Evidences",
+                column: "ImageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Evidences_Image_ImageId",
+                table: "Evidences",
+                column: "ImageId",
+                principalTable: "Image",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Evidences_Image_ImageId",
+                table: "Evidences");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Evidences_ImageId",
+                table: "Evidences");
+
+            migrationBuilder.DropColumn(
+                name: "EdgeBoxPath",
+                table: "Evidences");
+
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                table: "Evidences");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Uri",
+                table: "Evidences",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
