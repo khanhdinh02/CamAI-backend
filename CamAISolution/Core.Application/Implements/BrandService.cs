5f9eef0e119d7daa5cfb46ebd3ba256aa1600948
duy <nguyenduduy233@gmail.com>
2024-03-12 17:03:39 +0700

    #199 Fix delete bug

--- CamAISolution/Core.Application/Implements/BrandService.cs
+++ CamAISolution/Core.Application/Implements/BrandService.cs
@@ -178,7 +178,7 @@ public class BrandService(
         if (brand == null)
             return;
 
-        if (brand.BrandManager == null || brand.BrandManager.AccountStatus == AccountStatus.New)
+        if (brand.BrandManagerId == null)
         {
             unitOfWork.Brands.Delete(brand);
             await unitOfWork.CompleteAsync();
