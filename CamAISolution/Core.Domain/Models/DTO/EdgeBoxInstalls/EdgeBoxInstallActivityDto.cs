da5b4df54855567ac24b9e074cf248feba42124e
duy <nguyenduduy233@gmail.com>
2024-03-27 09:48:59 +0700

    #559 Add DTO

--- CamAISolution/Core.Domain/Models/DTO/EdgeBoxInstalls/EdgeBoxInstallActivityDto.cs
+++ CamAISolution/Core.Domain/Models/DTO/EdgeBoxInstalls/EdgeBoxInstallActivityDto.cs
@@ -1,6 +1,13 @@
+using Core.Domain.Enums;
+
 namespace Core.Domain.DTO;
 
-public class EdgeBoxInstallActivityDto : BaseDto
+public class EdgeBoxInstallActivityDto
 {
-    // TODO:
+    public Guid Id { get; set; }
+    public Guid ModifiedById { get; set; }
+    public DateTime ModifiedTime { get; set; }
+    public Guid EdgeBoxInstallId { get; set; }
+    public EdgeBoxInstallStatus OldStatus { get; set; }
+    public EdgeBoxInstallStatus NewStatus { get; set; }
 }
