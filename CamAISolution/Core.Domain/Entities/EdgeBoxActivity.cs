6d08d4693b97ba45362a0ddf50767d8534d73144
Tran Dinh Khanh <khanhdinh02@yahoo.com>
2024-04-01 01:35:06 +0700

    Merge EdgeBoxInstallActivity to EdgeBoxActivity

--- CamAISolution/Core.Domain/Entities/EdgeBoxActivity.cs
+++ CamAISolution/Core.Domain/Entities/EdgeBoxActivity.cs
@@ -5,9 +5,13 @@ namespace Core.Domain.Entities;
 
 public class EdgeBoxActivity : ActivityEntity
 {
-    public Guid EdgeBoxId { get; set; }
-    public EdgeBoxStatus? OldStatus { get; set; } = null!;
-    public EdgeBoxStatus? NewStatus { get; set; } = null!;
+    public Guid? EdgeBoxId { get; set; }
+    public Guid? EdgeBoxInstallId { get; set; }
+    public EdgeBoxActivityType Type { get; set; }
 
-    public virtual EdgeBox EdgeBox { get; set; } = null!;
+    // public EdgeBoxStatus? OldStatus { get; set; } = null!;
+    // public EdgeBoxStatus? NewStatus { get; set; } = null!;
+
+    public virtual EdgeBox? EdgeBox { get; set; }
+    public virtual EdgeBoxInstall? EdgeBoxInstall { get; set; }
 }
---
+++ CamAISolution/Core.Domain/Enums/EdgeBoxActivityType.cs
@@ -0,0 +1,10 @@
+namespace Core.Domain.Enums;
+
+public enum EdgeBoxActivityType
+{
+    Installed = 0,
+    Uninstalled = 1,
+    Activated = 2,
+    Healthy = 3,
+    Unhealthy = 4
+}
