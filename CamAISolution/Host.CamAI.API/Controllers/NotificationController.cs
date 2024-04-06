5c2b7803f0c1cfe4eaf35c049cf2caf90a064fd7
Truong Tien Dat <dd220302@gmail.com>
2024-04-06 07:43:11 +0700

    add update fcm and fix mapping of notification

--- CamAISolution/Host.CamAI.API/Controllers/NotificationController.cs
+++ CamAISolution/Host.CamAI.API/Controllers/NotificationController.cs
@@ -23,11 +23,22 @@ public class NotificationsController(INotificationService notificationService, I
         [FromQuery] SearchNotificationRequest req
     ) => mapping.Map<AccountNotification, NotificationDto>(await notificationService.SearchNotification(req));
 
+    /// <summary>
+    /// Update status of notification
+    /// </summary>
+    /// <param name="notificationId"></param>
+    /// <param name="status"></param>
+    /// <returns></returns>
     [HttpPatch("{notificationId}/status/{status}")]
-    public async Task<NotificationDto> UpdateNotificationStatus(Guid notificationId, NotificationStatus status)
+    public async Task<ActionResult<NotificationDto>> UpdateNotificationStatus(
+        Guid notificationId,
+        NotificationStatus status
+    )
     {
-        return mapping.Map<AccountNotification, NotificationDto>(
-            await notificationService.UpdateStatus(notificationId, status)
+        return Ok(
+            mapping.Map<AccountNotification, NotificationDto>(
+                await notificationService.UpdateStatus(notificationId, status)
+            )
         );
     }
 }
