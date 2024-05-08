a6aaad1e5c00c3435d2ab2d779a43b26a0dc3dde
Truong Tien Dat <dd220302@gmail.com>
2024-05-09 02:29:49 +0700

    update phone regex

--- CamAISolution/Core.Domain/Constants/RegexHelper.cs
+++ CamAISolution/Core.Domain/Constants/RegexHelper.cs
@@ -7,8 +7,8 @@ public static class RegexHelper
     /// <summary>
     /// <see cref="https://stackoverflow.com/a/3216204/19327184"/>
     /// </summary>
-    public static readonly Regex PascalSplitting = new(@"(?<=[A-Z])(?=[A-Z][a-z])|(?<=[^A-Z])(?=[A-Z])|(?<=[A-Za-z])(?=[^A-Za-z])");
+    public static readonly Regex PascalSplitting =
+        new(@"(?<=[A-Z])(?=[A-Z][a-z])|(?<=[^A-Z])(?=[A-Z])|(?<=[A-Za-z])(?=[^A-Za-z])");
 
-    public static readonly Regex VietNamPhoneNumber = new(@"(03|05|07|08|09|01[2|6|8|9])+([0-9]{8})\b");
-
+    public static readonly Regex VietNamPhoneNumber = new(@"(\+84|84|0[3|5|7|8|9])+([0-9]{8})\b");
 }
